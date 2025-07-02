c2805057d5d7f7179d65bd64eaa3e3543e8c07e6
SudeshKumar-92 <Sudeshkumar1992june@gmail.com>
2025-07-02 16:50:53 +0530

    Delete ToDoList directory

--- ToDoList/home/models.py
+++
@@ -1,9 +0,0 @@
-from datetime import timezone
-from django.db import models
-from django.utils import timezone
-import datetime
-# Create your models here.
-class NewTask(models.Model):
-    AddTask=models.CharField(max_length=30)
-    time=models.TimeField(auto_now=False, auto_now_add=False)
-
